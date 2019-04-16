c76637da0d384d58be19b5548ca2f7c38c42c133
joost aafjes <joostaafjes@gmail.com>
2019-04-16 10:28:56 +0200

    add Udacity solution

--- main.cpp
+++ main.cpp
@@ -52,6 +52,30 @@ vector<double> JMT(vector<double> &start, vector<double> &end, double T) {
   Vector3f x = A.colPivHouseholderQr().solve(b);
 
   return {s_i, s_i_dot, 0.5 * s_i_dotdot, x[0], x[1], x[2]};
+  /*
+   * Udacity solution:
+   * MatrixXd A = MatrixXd(3, 3);
+  A << T*T*T, T*T*T*T, T*T*T*T*T,
+       3*T*T, 4*T*T*T,5*T*T*T*T,
+       6*T, 12*T*T, 20*T*T*T;
+
+  MatrixXd B = MatrixXd(3,1);
+  B << end[0]-(start[0]+start[1]*T+.5*start[2]*T*T),
+       end[1]-(start[1]+start[2]*T),
+       end[2]-start[2];
+
+  MatrixXd Ai = A.inverse();
+
+  MatrixXd C = Ai*B;
+
+  vector <double> result = {start[0], start[1], .5*start[2]};
+
+  for(int i = 0; i < C.size(); ++i) {
+    result.push_back(C.data()[i]);
+  }
+
+  return result;
+   */
 }
 
 int main() {
